c8f4aeadee1f44eefc4fff6f850e528a36449215
junsunna <91936830+junsunna@users.noreply.github.com>
2025-03-22 04:19:52 +0900

    [Silver V] Title: 집합, Time: 580 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,61 +1,42 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bitset>
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	vector<int> S;
-	string order;
-	int M, x;;
-	cin >> M;
-	while (M--) {
-		cin >> order;
-		if (order == "add") {
-			cin >> x;
-			sort(S.begin(), S.end());
-			if (!binary_search(S.begin(), S.end(), x)) {
-				S.push_back(x);
-			}
-		}
-		else if (order == "remove") {
-			cin >> x;
-			sort(S.begin(), S.end());
-			if (binary_search(S.begin(), S.end(), x)) {
-				S.erase(lower_bound(S.begin(), S.end(), x));
-			}
-		}
-		else if (order == "check") {
-			cin >> x;
-			sort(S.begin(), S.end());
-			if (binary_search(S.begin(), S.end(), x)) {
-				cout << 1 << "\n";
-			}
-			else {
-				cout << 0 << "\n";
-			}
-		}
-		else if (order == "toggle") {
-			cin >> x;
-			sort(S.begin(), S.end());
-			if (binary_search(S.begin(), S.end(), x)) {
-				S.erase(lower_bound(S.begin(), S.end(), x));
-			}
-			else {
-				S.push_back(x);
-			}
-		}
-		else if (order == "all") {
-			S.clear();
-			for (int i = 1; i <= 20; i++) {
-				S.push_back(i);
-			}
-		}
-		else if (order == "empty") {
-			S.clear();
-		}
-	}
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	return 0;
+    bitset<21> S;
+    string order;
+    int M, x;
+
+    cin >> M;
+    while (M--) {
+        cin >> order;
+
+        if (order == "add") {
+            cin >> x;
+            S.set(x);  // x 위치를 1로 설정 (O(1))
+        }
+        else if (order == "remove") {
+            cin >> x;
+            S.reset(x);  // x 위치를 0으로 설정 (O(1))
+        }
+        else if (order == "check") {
+            cin >> x;
+            cout << S.test(x) << "\n";  // x 위치의 값 출력 (O(1))
+        }
+        else if (order == "toggle") {
+            cin >> x;
+            S.flip(x);  // x 위치를 반전 (O(1))
+        }
+        else if (order == "all") {
+            S.set();  // 전체를 1로 설정 (O(1))
+        }
+        else if (order == "empty") {
+            S.reset();  // 전체를 0으로 설정 (O(1))
+        }
+    }
+
+    return 0;
 }
