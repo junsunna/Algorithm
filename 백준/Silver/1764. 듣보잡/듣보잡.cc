a5898ce0a54703e634cb65d32929c1eed24ea3f1
junsunna <91936830+junsunna@users.noreply.github.com>
2025-03-22 04:44:16 +0900

    [Silver IV] Title: 듣보잡, Time: 108 ms, Memory: 10164 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	vector<string> v;
+	int N, M, cnt = 0;
+	vector<string> answer;
+	cin >> N >> M;
+	for (int i = 0; i < N + M; i++) {
+		string name;
+		cin >> name;
+		v.push_back(name);
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < N + M - 1; i++) {
+		if (v[i] == v[i + 1]) {
+			answer.push_back(v[i]);
+			cnt++;
+			i++;
+		}
+	}
+	cout << cnt << "\n";
+	for (auto& p : answer) {
+		cout << p << "\n";
+	}
+
+	return 0;
+}
