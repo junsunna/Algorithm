2a798ce0ce38a67e69973e574cb6f73e9eea377b
junsunna <91936830+junsunna@users.noreply.github.com>
2025-04-05 23:52:45 +0900

    [Silver II] Title: 연속합, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 int dp[100000] = { 0 };
 
@@ -7,14 +6,27 @@ int main() {
 	int n;
 	cin >> n;
 	cin >> dp[0];
-	int maxSum = dp[0];
+	int max = dp[0];
 	for (int i = 1; i < n; i++) {
 		int num;
 		cin >> num;
-		dp[i] = max(dp[i - 1] + num, num);
-		maxSum = max(maxSum, dp[i]);
+		int a = dp[i - 1];
+		if (a < 0) {
+			if (num > 0) {
+				dp[i] = num;
+				continue;
+			}
+		}
+		if (dp[i - 1] + num > 0) {
+			dp[i] = dp[i - 1] + num;
+		}
+		else {
+			dp[i] = num;
+		}
+        if (dp[i] > max)
+            max = dp[i];
 	}
-	cout << maxSum;
+	cout << max;
 
 	return 0;
 }
