bfe57a6e22953ada3549f7d5cb75b36da8e82231
junsunna <91936830+junsunna@users.noreply.github.com>
2025-04-07 00:14:49 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
-int main(){
+
+int main() {
     int n;
     cin >> n;
-    long long dp[91]; // 범위 초과 방지
-    dp[0] = 0;
+    long long dp[91];
     dp[1] = 1;
-    for (int i = 2; i <= n; i++){
-        dp[i] = dp[i - 1] + dp[i - 2]; // 그 전 값 + 그 전전 값
+    dp[2] = 1;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
     }
     cout << dp[n];
+    
     return 0;
 }
