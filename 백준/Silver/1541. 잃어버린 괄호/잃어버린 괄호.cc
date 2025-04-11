b42cd860568448072f261288eeca93d91db776e8
junsunna <91936830+junsunna@users.noreply.github.com>
2025-04-11 23:39:44 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,62 +1,27 @@
 #include <iostream>
 #include <string>
-#include <stack>
-using namespace std;
-
-string expression;
-
 int main() {
-	cin >> expression;
-	stack<int> s;
-	int size = expression.size();
+	std::string s;
+	std::cin >> s;
+	int size = s.size();
 	int lastIndex = 0;
-	int num;
-	int sum = 0;
-	int result = 0;
-	int toggle = 0;
+	int num, sum = 0, result = 0;
+	bool toggle = 0;
 	while (lastIndex < size) {
 		int len = 0;
-		
-		for (int i = lastIndex; i < size; i++) {
-			if (expression[i] >= '0') {
+		for (int i = lastIndex; i < size; i++) 
+			if (s[i] >= '0' && s[i] <= '9') 
 				len++;
-			}
 			else break;
-		}
-		num = stoi(expression.substr(lastIndex, len));
-		lastIndex += len;
-		if (lastIndex == size) {
-			if (expression[lastIndex - len - 1] == '-') {
-				result -= num;
-			}
-			else {
-				if (toggle == 1) {
-					result -= num;
-				}
-				else {
-					result += num;
-				}
-			}
-			continue;
-		}
-		if (lastIndex - len - 1 >= 0 && expression[lastIndex - len - 1] == '+') {
-			if (toggle == 1) {
-				result -= num;
-			}
-			else {
-				result += num;
-			}
-		}
-		else if (lastIndex - len - 1 >= 0 && expression[lastIndex - len - 1] == '-') {
-			result -= num;
-			if (toggle == 0)
-				toggle ^= 1;
-		}
-		else result += num;
-		lastIndex++;
+		num = std::stoi(s.substr(lastIndex, len));
+		if (!toggle && lastIndex - 1 > 0 && s[lastIndex - 1] == '-') toggle = 1;
+		if (toggle == 0) 
+			result += num;
+		else 
+			sum += num;
+		lastIndex += len + 1;
 	}
-
-	result += sum;
+	result -= sum;
 	std::cout << result;
 	return 0;
 }
