af188e0fce0464bce92832204f9a070c0b9e4afe
junsunna <91936830+junsunna@users.noreply.github.com>
2025-06-05 15:36:40 +0900

    [Bronze II] Title: Taro's Shopping, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14969. Taro's Shopping/Taro's Shopping.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie();
+    int n, m;
+    while (true) {
+        cin >> n >> m;
+        if (n == 0 && m == 0) return 0;
+        vector<int> v;
+        v.resize(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+        int a, b;
+        int max = 0;
+        bool result = false;
+        for (int i = n - 1; i > 0; i--) {
+            a = v[i];
+            for (int j = i - 1; j >= 0; j--) {
+                b = v[j];
+                if (a + b <= m && max < a + b) {
+                    max = a + b;
+                    result = true;
+                }
+            }
+        }
+        if (!result) cout << "NONE" << '\n';
+        else cout << max << '\n';
+    }
+    return 0;
+}
