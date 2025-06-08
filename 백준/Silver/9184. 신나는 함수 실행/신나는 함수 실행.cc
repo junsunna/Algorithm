b0d1a34aeab9ba93bd1426b8658e6a38237a6fa3
junsunna <91936830+junsunna@users.noreply.github.com>
2025-06-08 19:34:58 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 260 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -2,43 +2,49 @@
 #include <math.h>
 using namespace std;
 
-int dp[51][51][51];
+int dp[21][21][21];
 
 int w(int a, int b, int c) {
-	if (dp[a][b][c] != -1) return dp[a][b][c];
+
 	if (a <= 0 || b <= 0 || c <= 0) return 1;
-	if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
-	if (a < b && b < c) return w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
-	dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+	if (a > 20 || b > 20 || c > 20) return dp[20][20][20];
 	return dp[a][b][c];
+}
+
+void bdp() {
+	for (int i = 1; i <= 20; i++) {
+		for (int j = 1; j <= 20; j++) {
+			for (int k = 1; k <= 20; k++) {
+				if (i < j && j < k) {
+					dp[i][j][k] = dp[i][j][k - 1] + dp[i][j - 1][k-1] - dp[i][j-1][k];
+				}
+				else {
+					dp[i][j][k] = dp[i - 1][j][k] + dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] - dp[i - 1][j - 1][k - 1];
+				}
+			}
+		}
+	}
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie();
-	for (int i = 0; i <= 50; i++) {
-		for (int j = 0; j <= 50; j++) {
-			for (int k = 0; k <= 50; k++) {
-				
-				dp[i][j][k] = -1;
-				if (i > 20 || j > 20 || k > 20)
-					dp[i][j][k] = 1048576;
-				else if (i <= j && i <= k) {
-					dp[i][j][k] = pow(2, i);
-				}
-			}
+	cout.tie();
+	
+	for (int i = 0; i <= 20; i++) {
+		for (int j = 0; j <= 20; j++) {
+			dp[0][i][j] = 1;
+			dp[i][0][j] = 1;
+			dp[i][j][0] = 1;
 		}
 	}
+	bdp();
+
 	while (1) {
 		int a, b, c;
 		cin >> a >> b >> c;
 		if (a == -1 && b == -1 && c == -1) break;
-		if (a < 1 || b < 1 || c < 1) {
-			cout << "w(" << a << ", " << b << ", " << c << ") = " << 1 << "\n";
-			continue;
-		}
-		int result = w(a, b, c);
-		cout << "w(" << a << ", " << b << ", " << c << ") = " << result << "\n";
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << "\n";
 	}
 	return 0;
 }
