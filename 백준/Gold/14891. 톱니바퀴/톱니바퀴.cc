efce8da05cfbd907ff9d0cb530c6758c58517d20
junsunna <91936830+junsunna@users.noreply.github.com>
2025-06-26 18:59:32 +0900

    [Gold V] Title: 톱니바퀴, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
@@ -9,45 +9,38 @@ void rotate(int n, int d) {
 		top[n] = (top[n] + 7) % 8;
 	else
 		top[n] = (top[n] + 1) % 8;
-
 }
 
-bool isDifferent(int left, int right, int curTop[]) {
-	int l_right = (curTop[left] + 2) % 8;
-	int r_left = (curTop[right] + 6) % 8;
+bool isDifferent(int left, int right) {
+	int l_right = (top[left] + 2) % 8;
+	int r_left = (top[right] + 6) % 8;
+
 	return grid[left][l_right] != grid[right][r_left];
 }
 
 void move(int n, int d)
 {
-	int dir[5] = { 0 }; // 톱니바퀴 1~4의 회전 방향 저장
+	int dir[5] = { 0 };
 	dir[n] = d;
 
-	int curTop[5];
-	for (int i = 1; i <= 4; i++) curTop[i] = top[i];
-
-	// 왼쪽으로 전파
 	for (int i = n; i > 1; --i)
-		if (isDifferent(i - 1, i, curTop)) 
+		if (isDifferent(i - 1, i)) 
 			dir[i - 1] = -dir[i];
 		else break;
 
-	// 오른쪽으로 전파
 	for (int i = n; i < 4; ++i)
-		if (isDifferent(i, i + 1, curTop))
+		if (isDifferent(i, i + 1))
 			dir[i + 1] = -dir[i];
 		else break;
 
-	// 회전 수행
 	for (int i = 1; i <= 4; ++i)
 		if (dir[i]) rotate(i, dir[i]);
 }
 
 int cal() {
 	int sum = 0;
-	for (int i = 1; i <= 4; i++) {
+	for (int i = 1; i <= 4; i++) 
 		if (grid[i][top[i]] == 1) sum += (1 << (i - 1));
-	}
 
 	return sum;
 }
