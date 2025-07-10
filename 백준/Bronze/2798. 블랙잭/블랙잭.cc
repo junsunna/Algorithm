a67b6d4a690714043eeeb77da15c3f922d53d636
junsunna <91936830+junsunna@users.noreply.github.com>
2025-07-10 13:53:05 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,28 +1,25 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int N, M;
-int arr[100];
-int sum = 0;
+int arr[101];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
 	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
+	for (int i = 1; i <= N; i++) {
 		cin >> arr[i];
 	}
-
-	for (int i = 0; i < N - 2; i++) {
-		for (int j = i + 1; j < N - 1; j++) {
-			for (int k = j + 1; k < N; k++) {
-				if (arr[i] + arr[j] + arr[k] <= M) {
-					sum = max(arr[i] + arr[j] + arr[k], sum);
+	int max = 0;
+	for (int i = 1; i <= N; i++) {
+		for (int j = i + 1; j <= N; j++) {
+			for (int k = j + 1; k <= N; k++) {
+				int sum = arr[i] + arr[j] + arr[k];
+				if (sum <= M && sum > max) {
+					max = sum;
 				}
 			}
 		}
 	}
-	cout << sum;
+	cout << max;
 	return 0;
 }
